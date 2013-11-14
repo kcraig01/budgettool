94d2e478a1dca851164da34d509d551a5408db88
Kerry <kcraig01@gmail.com>
2013-11-13 23:05:22 -0700

    updated

--- oauth.js
+++ oauth.js
@@ -2,12 +2,12 @@ var ids = {
 facebook: {
    clientID: '1393055207600117',
    clientSecret: '94a80968d62b4607904c475bce9f79ad',
-   callbackURL: 'saveyourself.herokuapp.com/auth/facebook/callback'
+   callbackURL: 'http://saveyourself.herokuapp.com/auth/facebook/callback'
 },
 
 google: {
- returnURL: 'saveyourself.herokuapp.com/auth/google/callback',
- realm: 'saveyourself.herokuapp.com'
+ returnURL: 'http://saveyourself.herokuapp.com/auth/google/callback',
+ realm: 'http://saveyourself.herokuapp.com/'
 }
 }
 
